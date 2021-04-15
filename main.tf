c74e2c2adeab43df058ae1a80e5c795e7c39b7ec
Karl Kirch <karlkrch@gmail.com>
2021-04-15 16:06:22 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      configuration_aliases = [
+        aws.backup,
+        aws.prod
+      ]
+    }
+  }
+}
+
 resource "null_resource" "test" {}
 
 resource "null_resource" "test_3" {}
