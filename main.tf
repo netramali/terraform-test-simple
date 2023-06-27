6f857ff16a0208b945429499bb9b40efde213200
rberecka <rachael.berecka@hashicorp.com>
2023-06-27 16:15:22 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,10 +10,6 @@ terraform {
   }
 }
 
-resource "null_resource" "test" {}
+resource "null_resource" "test_1" {}
 
 resource "null_resource" "test_2" {}
-
-resource "null_resource" "test_3" {}
-
-resource "null_resource" "test_4" {}
